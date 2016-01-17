fbcd918de30d2884bdee33c5b7c364fb7c03a7e3
r4geee <alexei.rosto@gmail.com>
2016-01-17 16:56:06 +0200

    shortener task 3 done

--- src/com/javarush/test/level33/lesson15/big01/Shortener.java
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -1,12 +1,29 @@
 package com.javarush.test.level33.lesson15.big01;
 
+import com.javarush.test.level33.lesson15.big01.strategies.StorageStrategy;
+
 public class Shortener {
 
-    public Long getId(String string) {
-        return 0L;
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string))
+            return storageStrategy.getKey(string);
+        lastId++;
+        storageStrategy.put(lastId, string);
+        return lastId;
+    }
+
+    public synchronized String getString(Long id) {
+        if (storageStrategy.containsKey(id)){
+            return storageStrategy.getValue(id);
+        }
         return null;
     }
+
 }
