d54f7dc5e8ab440d1cec87b32456b48192068d56
r4geee <alexei.rosto@gmail.com>
2016-01-17 16:21:38 +0200

    shortener task 1 done

---
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level33.lesson15.big01;
+
+public class Shortener {
+
+    public Long getId(String string) {
+        return 0L;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
