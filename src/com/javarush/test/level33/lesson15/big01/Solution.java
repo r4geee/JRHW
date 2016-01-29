8fe83af0beebef3bc5adcd172edf63d24e8f96ca
r4geee <alexei.rosto@gmail.com>
2016-01-29 14:40:05 +0200

    shortener task 12 done

--- src/com/javarush/test/level33/lesson15/big01/Solution.java
+++ src/com/javarush/test/level33/lesson15/big01/Solution.java
@@ -17,6 +17,7 @@ public class Solution {
         //testStrategy(new FileStorageStrategy(), 1000);
         testStrategy(new OurHashBiMapStorageStrategy(), 10000);
         testStrategy(new HashBiMapStorageStrategy(), 10000);
+        testStrategy(new DualHashBidiMapStorageStrategy(), 10000);
     }
 
     public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
