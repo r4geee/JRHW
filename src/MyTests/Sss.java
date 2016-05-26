04a3cf2692c0aed9eb492c74ec23122d62eb7f9f
r4geee <alexei.rosto@gmail.com>
2016-05-26 14:15:15 +0300

    lvl34 big-11 done

--- src/MyTests/Sss.java
+++ src/MyTests/Sss.java
@@ -1,6 +1,8 @@
 package MyTests;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +10,10 @@ import java.util.List;
  * Created by Alexei on 17.01.2016.
  */
 public class Sss {
-    public static void main(String[] args) {
-        int i = -7 % 6;
-        System.out.println(i);
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/DO NOT READ.txt");
+        byte[] bytes = new byte[fileInputStream.available()];
+        fileInputStream.read(bytes);
+        System.out.println(new String(bytes));
     }
 }
--- src/com/javarush/test/level34/lesson15/big01/model/Model.java
+++ src/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -2,12 +2,35 @@ package com.javarush.test.level34.lesson15.big01.model;
 
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
 
+import java.nio.file.Paths;
+
 public class Model {
     public static final int FIELD_SELL_SIZE = 20;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("src/com/javarush/test/level34/lesson15/big01/res/levels.txt"));
     EventListener eventListener;
 
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restart();
+    }
+
 }
