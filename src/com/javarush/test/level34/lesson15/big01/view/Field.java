82e6e01d08d10d561cc75d31eed80648000217aa
r4geee <alexei.rosto@gmail.com>
2016-05-26 16:12:53 +0300

    lvl34 big-14 done

--- src/com/javarush/test/level34/lesson15/big01/view/Field.java
+++ src/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,41 +1,57 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
-
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
-import com.javarush.test.level34.lesson15.big01.model.*;
-import com.javarush.test.level34.lesson15.big01.model.Box;
+import com.javarush.test.level34.lesson15.big01.model.Direction;
+import com.javarush.test.level34.lesson15.big01.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
 
-    EventListener eventListener;
+    private EventListener eventListener;
     private View view;
 
     public Field(View view) {
         this.view = view;
+        addKeyListener(new KeyHandler());
+        setFocusable(true);
     }
 
     public void paint(Graphics graphics) {
-//        Box box = new Box(50, 50);
-//        box.draw(graphics);
-//        Player player = new Player(100, 100);
-//        player.draw(graphics);
-//        Home home = new Home(100, 100);
-//        home.draw(graphics);
-//        Wall wall = new Wall(150, 100);
-//        wall.draw(graphics);
         graphics.setColor(Color.BLACK);
-        //graphics.drawRect(0, 0, getWidth(), getHeight());
         graphics.fillRect(0, 0, getWidth(), getHeight());
-        for(GameObject gameObject : view.getGameObjects().getAll()) {
+        for (GameObject gameObject : view.getGameObjects().getAll()) {
             gameObject.draw(graphics);
         }
     }
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case KeyEvent.VK_UP:
+                    eventListener.move(Direction.UP);
+                    break;
+                case KeyEvent.VK_R:
+                    eventListener.restart();
+                    break;
+            }
+        }
     }
 }
